d0dd67afa7248b4fa46e385c3cf53eede494ab9f
inHoo <59555684+inhoo-han@users.noreply.github.com>
2023-10-21 00:03:11 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.07 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] ingr) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < ingr.length; i++) {
+            sb.append(ingr[i]);
+            if(sb.length() > 3 && sb.subSequence(sb.length() - 4, sb.length()).equals("1231")) {
+                answer++;
+                sb.delete(sb.length() - 4, sb.length());
+            }
+        }
+                
+        
+        return answer;
+    }
+}
